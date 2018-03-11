d945fdf7abd5d08700fd05fea2bbc4a24d20c34a
lydivetteroldan <lydivette.roldan@gmail.com>
2018-03-10 19:15:53 -0500

    Add `Create` Post curl script
    Curl script is working in local environment.

---
+++ assets/scripts/json/create-post.sh
@@ -0,0 +1,14 @@
+curl "http://localhost:4741/posts" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "post": {
+      "title": "'"${TITLE}"'",
+      "content": "'"${CONTENT}"'",
+      "date": "'"${DATE}"'"
+    }
+  }'
+
+echo
