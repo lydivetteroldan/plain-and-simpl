e9601e03c498ee85cb1bc79706ab3eb423769f56
lydivetteroldan <lydivette.roldan@gmail.com>
2018-03-11 21:42:44 -0400

    Clean `index.js`

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,18 +2,21 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+
 const authEvents = require('./auth/events')
 const postEvents = require('./posts/events')
-const signedInSettings = $('#signedInButton')
 
 function notSignedIn () {
-  $(signedInSettings).hide()
+  $('#signedInButton').hide()
 }
 
 notSignedIn()
 
 $(() => {
   setAPIOrigin(location, config)
+})
+
+$(() => {
   authEvents.eventHandlers()
   postEvents.eventHandlers()
 })
