9fbd4d354a17bdc8be6714d5b7c65484b4d9c224
lydivetteroldan <lydivette.roldan@gmail.com>
2018-03-13 21:42:59 -0400

    Display and hide `Welcome` message after Sign Up, Sign In and Sign Out actions

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -6,6 +6,10 @@ const signUpSuccess = function (data) {
   $('#signUpMessage').text('Signed up successfully')
   $('#signUpMessage').css('color', 'green')
   $('#signUpButton').hide(200)
+  $('#signInButton').hide(200)
+  $('#signedInButton').show(200)
+  $('.section.welcome').hide(200)
+  store.user = data.user
 }
 
 const signUpFailure = function () {
@@ -20,6 +24,7 @@ const signInSuccess = function (data) {
   $('#signInButton').hide(200)
   $('#signUpButton').hide(200)
   $('#signedInButton').show(200)
+  $('.section.welcome').hide(200)
   store.user = data.user
 }
 
@@ -49,6 +54,7 @@ const signOutSuccess = function () {
   $('#signUpButton').show(200)
   $('#signedInButton').hide(200)
   $('#postsList').hide(200)
+  $('.section.welcome').show(200)
   store.user = null
 }
 
